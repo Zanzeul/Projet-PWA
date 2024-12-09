8454830c93b30bc99d1459ddf62631266ec37ab1
Zanzeul <athur.chancereul@gmail.com>
2024-12-09 22:08:35 +0100

    Menu nom playing movies

--- app/dashboard/design/ApplicationLayout.tsx
+++ app/dashboard/design/ApplicationLayout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 export const ApplicationLayout = (props: PropsWithChildren) => {
   return (
     <div
-      className="h-screen w-screen grid grid-rows-[60px_1fr] md:grid-cols-[250px_1fr] md:grid-rows-[60px_1fr] [grid-template-areas:'header_header''content_content'] md:[grid-template-areas:'header_header''sidebar_content']"
+      className="overflow-hidden h-screen w-screen grid grid-rows-[60px_1fr] md:grid-cols-[250px_1fr] md:grid-rows-[60px_1fr] [grid-template-areas:'header_header''content_content'] md:[grid-template-areas:'header_header''sidebar_content']"
     >
       {props.children}
     </div>
