bc2075d2b4df331e254b132c9a88bb66afd4390a
Zanzeul <athur.chancereul@gmail.com>
2024-12-09 22:48:53 +0100

    BUg barre

--- app/dashboard/design/ApplicationLayout.tsx
+++ app/dashboard/design/ApplicationLayout.tsx
@@ -1,11 +1,16 @@
+
 import { PropsWithChildren } from "react";
 
 export const ApplicationLayout = (props: PropsWithChildren) => {
   return (
     <div
-      className="h-screen w-screen grid grid-rows-[60px_1fr] md:grid-cols-[250px_1fr] 
-      md:grid-rows-[60px_1fr] [grid-template-areas:'header_header''content_content'] 
-      md:[grid-template-areas:'header_header''sidebar_content']"
+      className="
+        h-screen w-screen grid 
+        grid-rows-[60px_1fr_auto]   /* Par défaut, 60px pour le header, reste pour le contenu, et une ligne pour la sidebar */
+        md:grid-cols-[250px_1fr]   /* Sur desktop, sidebar à gauche */
+        md:grid-rows-[60px_1fr]    /* Sur desktop, pas de ligne pour la sidebar en bas */
+        [grid-template-areas:'header_header''content_content''sidebar_sidebar'] 
+        md:[grid-template-areas:'header_header''sidebar_content']"
     >
       {props.children}
     </div>
